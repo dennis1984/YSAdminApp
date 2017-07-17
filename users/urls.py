bdf5fd184a6e6e3367cda4182e67d3e1266127b9
dennis1984 <year2005706@163.com>
2017-07-17 16:43:41 +0800

    管理后台架构完善

--- users/urls.py
+++ users/urls.py
@@ -3,7 +3,6 @@ from django.conf.urls import url, include
 from rest_framework.routers import DefaultRouter
 from rest_framework.urlpatterns import format_suffix_patterns
 from users import views as users_view
-from users.wx_auth import views as wx_auth_views
 
 urlpatterns = [
     url(r'user_action/$', users_view.UserAction.as_view()),
