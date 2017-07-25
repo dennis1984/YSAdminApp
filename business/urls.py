eb3ae44f72c20bf652e72724bab2a60626cf4612
dennis1984 <year2005706@163.com>
2017-07-25 18:15:51 +0800

    商户管理url完善

--- business/urls.py
+++ business/urls.py
@@ -6,21 +6,23 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from business import views
 
 urlpatterns = [
-    url(r'city_action/$', views.CityAction.as_view()),
-    url(r'city_list/$', views.CityList.as_view()),
-    url(r'district_action/$', views.DistrictAction.as_view()),
+    url(r'^city_action/$', views.CityAction.as_view()),
+    url(r'^city_list/$', views.CityList.as_view()),
+    url(r'^district_action/$', views.DistrictAction.as_view()),
     url(r'district_list/$', views.DistrictAction.as_view()),
 
-    url(r'food_court_action/$', views.FoodCourtAction.as_view()),
-    url(r'food_court_list/$', views.FoodCourtList.as_view()),
+    url(r'^food_court_action/$', views.FoodCourtAction.as_view()),
+    url(r'^food_court_list/$', views.FoodCourtList.as_view()),
 
-    url(r'user_action/$', views.UserAction.as_view()),
-    url(r'user_list/$', views.UserList.as_view()),
+    url(r'^user_with_food_court_list/$', views.UserWithFoodCourtList.as_view()),
 
-    url(r'dishes_list/$', views.DishesList.as_view()),
-    url(r'dishes_action/$', views.DishesAction.as_view()),
+    url(r'^user_action/$', views.UserAction.as_view()),
+    url(r'^user_list/$', views.UserList.as_view()),
 
-    url(r'advert_picture_action/$', views.AdvertPictureAction.as_view()),
+    url(r'^dishes_list/$', views.DishesList.as_view()),
+    url(r'^dishes_action/$', views.DishesAction.as_view()),
+
+    url(r'^advert_picture_action/$', views.AdvertPictureAction.as_view()),
 
 
 ]
