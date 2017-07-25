670c84d66c5819a04a0af200c7b178418fc87983
dennis1984 <year2005706@163.com>
2017-07-25 16:18:52 +0800

    城市列表及美食城列表完善

--- business/forms.py
+++ business/forms.py
@@ -34,7 +34,7 @@ class DistrictDeleteForm(CityDeleteForm):
 
 
 class CityListForm(forms.Form):
-    city = forms.CharField(min_length=2, max_length=20)
+    city = forms.CharField(min_length=2, max_length=20, required=False)
     page_size = forms.IntegerField(min_value=1, required=False)
     page_index = forms.IntegerField(min_value=1, required=False)
 
