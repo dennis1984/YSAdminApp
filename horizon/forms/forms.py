58895386980d2b92e5228e5fb37dd4e7eb3497c5
dennis1984 <year2005706@163.com>
2017-07-31 17:54:39 +0800

    form表单完善

--- horizon/forms/forms.py
+++ horizon/forms/forms.py
@@ -1,5 +1,7 @@
 # -*- encoding: utf-8 -*-
 from django import forms
+from django.forms import ChoiceField
+from django.utils.encoding import force_text
 
 
 class Form(forms.Form):
@@ -17,4 +19,17 @@ class Form(forms.Form):
             else:
                 if cld[key] is None:
                     cld.pop(key)
+        return cld
+
+    @property
+    def cleaned_data(self):
+        cld = super(Form, self).cleaned_data
+        for key, value in self.declared_fields.items():
+            if isinstance(value, ChoiceField):
+                for key2, value2 in value.choices:
+                    if key2 == cld[key] or force_text(key2) == cld[key]:
+                        if isinstance(key2, int):
+                            cld[key] = int(key2)
+                        elif isinstance(key, float):
+                            cld[key] = float(key2)
         return cld
