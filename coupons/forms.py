e2227b16fd6974cdc8d5c00b020ee0532dcda350
dennis1984 <year2005706@163.com>
2017-08-18 21:01:36 +0800

    优惠券管理：开发完善

--- coupons/forms.py
+++ coupons/forms.py
@@ -12,11 +12,11 @@ class CouponsInputForm(forms.Form):
                              error_messages={'required': u'优惠券类别值只能在以下列表中，'
                                                          u'[10, 20, 100, 200]'})
     type_detail = forms.CharField(max_length=64, required=False)
-    service_ratio = forms.IntegerField(min_value=1, max_value=100,
+    service_ratio = forms.IntegerField(min_value=0, max_value=100,
                                        error_messages={'required': u'平台商承担优惠比例值不能为空',
-                                                       'max_value': u'service_ratio取值范围为1~100'})
-    business_ratio = forms.IntegerField(min_value=1, max_value=100,
+                                                       'max_value': u'service_ratio取值范围为0~100'})
+    business_ratio = forms.IntegerField(min_value=0, max_value=100,
                                         error_messages={'required': u'商户承担优惠比例值不能为空',
-                                                        'max_value': u'business_ratio取值范围为1~100'})
+                                                        'max_value': u'business_ratio取值范围为0~100'})
 
 
