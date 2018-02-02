8373c3aa8721cba64b35bc12931ae53bbdedacb1
dennis <dennis@dennisdeiMac.local>
2018-02-02 16:56:58 +0800

    取消未核销订单向用户发短信: BUG修改

--- horizon/models.py
+++ horizon/models.py
@@ -4,10 +4,10 @@ from __future__ import unicode_literals
 from itertools import chain
 from django.db import models
 from django.conf import settings
-from horizon.main import timezoneStringTostring
 import urllib
 import os
 import copy
+import datetime
 
 
 def model_to_dict(instance, fields=None, exclude=None):
@@ -86,3 +86,23 @@ def get_perfect_detail_by_detail(cls, detail):
                                                            image_str.split('static/', 1)[1])
             detail_dict.pop(key)
     return detail_dict
+
+
+def timezoneStringTostring(timezone_string):
+    """
+    rest framework用JSONRender方法格式化datetime.datetime格式的数据时，
+    生成数据样式为：2017-05-19T09:40:37.227692Z 或 2017-05-19T09:40:37Z
+    此方法将数据样式改为："2017-05-19 09:40:37"，
+    返回类型：string
+    """
+    if not isinstance(timezone_string, (str, unicode)):
+        return ""
+    if not timezone_string:
+        return ""
+    timezone_string = timezone_string.split('.')[0]
+    timezone_string = timezone_string.split('Z')[0]
+    try:
+        timezone = datetime.datetime.strptime(timezone_string, '%Y-%m-%dT%H:%M:%S')
+    except:
+        return ""
+    return str(timezone)
